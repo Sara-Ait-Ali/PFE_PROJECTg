c8d784ce47ececd9c7482e6e8f0e183bab3a302b
Sara-Ait-Ali <saraaitali61@gmail.com>
2026-06-08 13:02:25 +0100

    errrrr

--- tmy_app/urls.py
+++ tmy_app/urls.py
@@ -6,8 +6,11 @@ from .views import (
     TMYInternalUpdateView,
     RegisterView,
 )
+
 urlpatterns = [
     path('submit/', TMYSubmitView.as_view()),
     path('status/<int:job_id>/', TMYStatusView.as_view()),
-
+    path('all/', TMYAllView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('update-internal/<int:job_id>/',TMYInternalUpdateView.as_view()),
 ]
